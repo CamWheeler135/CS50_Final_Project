a072b81035a4e63eca619bb8ce485ba976893295
Cameron Wheeler <98394754+CamWheeler135@users.noreply.github.com>
2022-12-16 10:18:46 +0000

    Adding action count plot function.

--- plotting.py
+++ plotting.py
@@ -1,8 +1,34 @@
 ''' Code that plots the performance of the algorithm. '''
 from pathlib import Path
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme(style="darkgrid")
 
 def create_directory(directory_path: str):
     ''' Makes a folder in the directory to place all of the plots. '''
     
     Path(directory_path).mkdir(parents=True, exist_ok=True)
 
+def cumulative_reward_plot(performance: dict, agent_type: str, save_path: str):
+    fig, ax = plt.subplots()
+    ax.plot(performance["cumulative_rewards"])
+    ax.set_ylabel("Cumulative Reward")
+    ax.set_xlabel("Time Steps")
+    ax.set_title(f"{agent_type} Performance")
+    plt.savefig(save_path)
+
+def actions_taken_plot(performance: dict, agent_type: str, save_path: str):
+    ''' Plots a bar chart of the times each action was taken. '''
+
+    x = [i for i in range(len(performance["arm_counter"]))]
+    y = performance["arm_counter"]
+
+    fig, ax = plt.subplots()
+    ax.bar(x, y, width=0.5)
+    ax.xaxis.get_major_locator().set_params(integer=True)
+    ax.set_ylabel("Times Chosen")
+    ax.set_xlabel("Bandit Choice")
+    ax.set_title(f"{agent_type} Action Selection")
+    plt.savefig(save_path)
+    
