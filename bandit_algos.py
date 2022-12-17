e30665c6e1749cad5afdab6d50854fa23b4269d6
Cameron Wheeler <98394754+CamWheeler135@users.noreply.github.com>
2022-12-17 22:52:09 +0000

    Addition of UCB algorithm

--- bandit_algos.py
+++ bandit_algos.py
@@ -19,7 +19,7 @@ class RandomAgent():
         self.pulled_arm_counter = np.zeros(self.env.num_of_bandits)
 
     def perform_actions(self):
-        ''' For each timestep, the agent will select and action. '''
+        ''' For each time step, the agent will select and action. '''
 
         for i in range(self.number_of_pulls):
             selected_action = np.random.choice(self.env.num_of_bandits)
@@ -60,7 +60,6 @@ class GreedyAgent():
         selected_action = np.random.choice(possible_actions[0])
         return selected_action
 
-
     def update_Q(self, selected_action, reward, action_count):
         ''' Updates the Q value for the action by averaging the immediate rewards
             over the times that action has been selected. '''
@@ -68,9 +67,8 @@ class GreedyAgent():
         updated_value = self.Q_values[selected_action] + 1 / action_count[selected_action] * (reward - self.Q_values[selected_action])
         self.Q_values[selected_action] = updated_value
 
-
     def perform_actions(self):
-        ''' For each timestep the agent will select an action according
+        ''' For each time step the agent will select an action according
             according to the highest action value (q_value) '''
 
         for i in range(self.number_of_pulls):
@@ -131,7 +129,7 @@ class EpsilonGreedyAgent():
         self.Q_values[selected_action] = updated_value
 
     def perform_actions(self):
-        ''' For each timestep the agent will select an action according
+        ''' For each time step the agent will select an action according
             according to the highest action value (q_value) but select a random
             action according to probability epsilon '''
 
@@ -151,3 +149,70 @@ class EpsilonGreedyAgent():
         return {"rewards": self.rewards, "cumulative_rewards": self.cumulative_reward, "arm_counter": self.pulled_arm_counter}
 
 
+
+
+
+class UCBAgent():
+    ''' Agent uses the upper confidence bounds to compute what actions to select. This agent uses the UCB1 algorithm. '''
+
+    def __init__(self, env, number_of_pulls) -> None:
+        self.env = env
+        self.number_of_pulls = number_of_pulls
+
+        # States internal of the agent. 
+        self.rewards = []
+        self.cumulative_reward = []
+        self.pulled_arm_counter = np.zeros(self.env.num_of_bandits)
+        # Values used in the UCB computation.
+        self.Q_values = np.zeros(self.env.num_of_bandits)
+        self.Ut_values = np.zeros(self.env.num_of_bandits)
+        self.UCB = np.zeros(self.env.num_of_bandits)
+        self.time_steps = 0
+        # Ensures all actions are selected once before using UCB.
+        self.first_action_counter = 0 
+
+    def update_confidence(self, selected_action, action_count, time_steps):
+        ''' Computes and updates the confidence bound for each bandit. '''
+
+        updated_confidence = np.sqrt((2 * np.log(time_steps)/ action_count[selected_action]))
+        self.Ut_values[selected_action] = updated_confidence
+
+    def update_Q(self, selected_action, reward, action_count):
+        ''' Updates the Q value for the action by averaging the immediate rewards
+            over the times that action has been selected. '''
+
+        updated_value = self.Q_values[selected_action] + 1 / action_count[selected_action] * (reward - self.Q_values[selected_action])
+        self.Q_values[selected_action] = updated_value
+
+    def select_action(self, first_action_counter):
+        ''' Selections actions by choosing the value that maximizes Qt + Ut. '''
+
+        # Choose each action once, then start using the UCB algorithm.
+        while first_action_counter < self.env.num_of_bandits:
+            return first_action_counter
+
+        # Compute the action for selection.
+        for bandit in range(self.env.num_of_bandits):
+            self.UCB[bandit] = self.Q_values[bandit] + self.Ut_values[bandit]
+        selected_action = np.argmax(self.UCB)
+        return selected_action
+
+    def perform_actions(self):
+        ''' For each time step the agent will select an action one time
+            before changing to the UCB algorithm. '''
+
+        for i in range(self.number_of_pulls):
+
+            selected_action = self.select_action(first_action_counter=self.first_action_counter)
+            reward, _, _, _ = self.env.step(selected_action)
+            self.rewards.append(reward)
+            self.cumulative_reward.append(sum(self.rewards) / len(self.rewards))
+            self.pulled_arm_counter[selected_action] += 1
+
+            # Update Q and Ut values
+            self.time_steps += 1
+            self.first_action_counter += 1
+            self.update_confidence(selected_action=selected_action, action_count=self.pulled_arm_counter, time_steps=self.time_steps)
+            self.update_Q(selected_action=selected_action, reward=reward, action_count=self.pulled_arm_counter)
+        
+        return {"rewards": self.rewards, "cumulative_rewards": self.cumulative_reward, "arm_counter": self.pulled_arm_counter}
