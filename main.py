edcabaac681322e616fa7e55769742bc56f31f82
Cameron Wheeler <98394754+CamWheeler135@users.noreply.github.com>
2022-12-23 12:25:22 +0000

    Addition of plotting function calls

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ env = BanditEnv(rewards=rewards, reward_probas=reward_probas)
 #---- Agent and Actions Set Up ----#
 
 # Create the agent
-random_agent = RandomAgent(env=env, number_of_pulls=50000)
-greedy_agent = GreedyAgent(env=env, number_of_pulls=50000)
-egreedy_agent = EpsilonGreedyAgent(env=env, number_of_pulls=50000) # Epsilon default value = 0.2.
-ucb_agent = UCBAgent(env=env, number_of_pulls=50000) # Default c hyperparameter = 2.
-ts_agent = ThompsonSamplingAgent(env=env, number_of_pulls=50000)
+random_agent = RandomAgent(env=env, number_of_pulls=1000)
+greedy_agent = GreedyAgent(env=env, number_of_pulls=1000)
+egreedy_agent = EpsilonGreedyAgent(env=env, number_of_pulls=1000) # Epsilon default value = 0.2.
+ucb_agent = UCBAgent(env=env, number_of_pulls=1000) # Default c hyperparameter = 2.
+ts_agent = ThompsonSamplingAgent(env=env, number_of_pulls=1000)
 
 # Tell the agent to perform its actions, this returns a dictionary of metrics.
 ra_performance = random_agent.perform_actions()
@@ -56,4 +56,11 @@ plotting.plot_all_single(performance=ega_performance, agent_type="Epsilon_Greedy
 plotting.plot_all_single(performance=ucb_performance, agent_type="UCB_Agent")
 plotting.plot_all_single(performance=ts_performance, agent_type="Thompson_Sampling_Agent")
 
+# Plotting comparative plots, comparing metrics from each agent together.
+agents_performance = [ra_performance, ga_performance, ega_performance, ucb_performance, ts_performance]
+names = ["Random Actions", 'Greedy Actions', 'epsilon-Greedy Actions', 'UCB', 'Thompson Sampling']
+plotting.cumulative_reward_compare_plot(agent_performance=agents_performance, names=names)
 
+agents_with_q = [ega_performance, ucb_performance, ga_performance]
+names = ['epsilon-Greedy', 'UCB', 'Greedy', 'True Values']
+plotting.action_value_comparison_plot(agent_performance=agents_with_q, names=names, true_vals=reward_probas)
