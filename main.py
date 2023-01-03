b38aee63fa7e64b561f437a7a019fd058ca445d9
Cameron Wheeler <98394754+CamWheeler135@users.noreply.github.com>
2023-01-03 21:53:48 +0000

    Addition of the Epsilon greedy decay instance and function calls.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 # Local Imports
 import plotting
 from bandit_env import BanditEnv
-from bandit_algos import RandomAgent, GreedyAgent, EpsilonGreedyAgent, UCBAgent, ThompsonSamplingAgent
+from bandit_algos import RandomAgent, GreedyAgent, EpsilonGreedyAgent, EGDecayAgent, UCBAgent, ThompsonSamplingAgent
 
 print("\nRunning Main.\n")
 
@@ -23,29 +23,29 @@ env = BanditEnv(rewards=rewards, reward_probas=reward_probas)
 #---- Agent and Actions Set Up ----#
 
 # Create the agent
-random_agent = RandomAgent(env=env, number_of_pulls=100000)
-greedy_agent = GreedyAgent(env=env, number_of_pulls=100000)
-egreedy_agent = EpsilonGreedyAgent(env=env, number_of_pulls=100000) # Epsilon default value = 0.2.
-ucb_agent = UCBAgent(env=env, number_of_pulls=100000) # Default c hyperparameter = 2.
-ts_agent = ThompsonSamplingAgent(env=env, number_of_pulls=100000)
+random_agent = RandomAgent(env=env, number_of_pulls=10000)
+greedy_agent = GreedyAgent(env=env, number_of_pulls=10000)
+egreedy_agent = EpsilonGreedyAgent(env=env, number_of_pulls=10000) # Epsilon default value = 0.2.
+egd_agent = EGDecayAgent(env=env, number_of_pulls=10000) # Epsilon default = 1, Epsilon decay rate default = 0.99, Epsilon min default = 0.1
+ucb_agent = UCBAgent(env=env, number_of_pulls=10000) # Default c hyperparameter = 2.
+ts_agent = ThompsonSamplingAgent(env=env, number_of_pulls=10000)
 
 # Tell the agent to perform its actions, this returns a dictionary of metrics.
 ra_performance = random_agent.perform_actions()
 ga_performance = greedy_agent.perform_actions()
 ega_performance = egreedy_agent.perform_actions()
+egda_performance = egd_agent.perform_actions()
 ucb_performance = ucb_agent.perform_actions()
 ts_performance = ts_agent.perform_actions()
 
-
 #---- Results and Plotting ----#
-
-# Creates a directory to store the plots of the agents performance.
 
 print("\nFinished Running, Loading Metrics and Plots.\n")
 
 print(f"Random Total Reward: {sum(ra_performance['rewards'])}")
 print(f"Greedy Total Reward: {sum(ga_performance['rewards'])}")
 print(f"Epsilon Greedy Total Reward: {sum(ega_performance['rewards'])}")
+print(f"Epsilon Greedy Decay Total Reward: {sum(egda_performance['rewards'])}")
 print(f"UCB Total Reward: {sum(ucb_performance['rewards'])}")
 print(f"Thompson Sampling Total Reward: {sum(ts_performance['rewards'])}")
 
@@ -53,12 +53,14 @@ print(f"Thompson Sampling Total Reward: {sum(ts_performance['rewards'])}")
 plotting.plot_all_single(ra_performance, agent_type="Random_Agent")
 plotting.plot_all_single(performance=ga_performance, agent_type="Greedy_Agent")
 plotting.plot_all_single(performance=ega_performance, agent_type="Epsilon_Greedy_Agent")
+plotting.plot_all_single(performance=egda_performance, agent_type="Epsilon_Greedy_with_Decay_Agent")
+plotting.plot_epsilon_decay(performance=egda_performance, agent_type="Epsilon_Greedy_with_Decay_Agent")
 plotting.plot_all_single(performance=ucb_performance, agent_type="UCB_Agent")
 plotting.plot_all_single(performance=ts_performance, agent_type="Thompson_Sampling_Agent")
 
 # Plotting comparative plots, comparing metrics from each agent together.
-agents_performance = [ra_performance, ga_performance, ega_performance, ucb_performance, ts_performance]
-names = ["Random Actions", 'Greedy Actions', 'epsilon-Greedy Actions', 'UCB', 'Thompson Sampling']
+agents_performance = [ra_performance, ga_performance, ega_performance, egda_performance, ucb_performance, ts_performance]
+names = ["Random Agent", 'Greedy Agent', 'epsilon-Greedy Agent', 'epsilon-Greedy Decay Agent', 'UCB Agent', 'Thompson Sampling Agent']
 plotting.cumulative_reward_compare_plot(agent_performance=agents_performance, names=names)
 
 agents_with_q = [ega_performance, ucb_performance, ga_performance]
