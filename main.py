19150681e3e42eef9aee98d01ec7b6cc214a225d
Cameron Wheeler <98394754+CamWheeler135@users.noreply.github.com>
2023-02-16 18:55:35 +0000

    Addition of Softmax agent function calls, slight adjustment to how the environement is created, it now initalises return probabilites at random

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ from pathlib import Path
 # Local Imports
 import plotting
 from bandit_env import BanditEnv
-from bandit_algos import RandomAgent, GreedyAgent, EpsilonGreedyAgent, EGDecayAgent, UCBAgent, ThompsonSamplingAgent
+from bandit_algos import RandomAgent, GreedyAgent, EpsilonGreedyAgent, EGDecayAgent, UCBAgent, ThompsonSamplingAgent, SoftMaxAgent
 
 print("\nRunning Main.\n")
 
 #---- Environment Set Up ----#
 
 # Set the rewards and return probabilites for each bandit
-rewards = np.array([1, 1, 1, 1, 1, 1, 1])
-reward_probas = np.array([0.6, 0.3, 0.5, 0.7, 0.2, 0.8, 0.3])
+number_of_bandits = 5
+rewards = np.ones(number_of_bandits)
+reward_probas = np.random.random(number_of_bandits)
 
 # Create the environment
 env = BanditEnv(rewards=rewards, reward_probas=reward_probas)
@@ -23,12 +24,13 @@ env = BanditEnv(rewards=rewards, reward_probas=reward_probas)
 #---- Agent and Actions Set Up ----#
 
 # Create the agent
-random_agent = RandomAgent(env=env, number_of_pulls=50000)
-greedy_agent = GreedyAgent(env=env, number_of_pulls=50000)
-egreedy_agent = EpsilonGreedyAgent(env=env, number_of_pulls=50000) # Epsilon default value = 0.2.
-egd_agent = EGDecayAgent(env=env, number_of_pulls=50000) # Epsilon default = 1, Epsilon decay rate default = 0.99, Epsilon min default = 0.1
-ucb_agent = UCBAgent(env=env, number_of_pulls=50000) # Default c hyperparameter = 2.
-ts_agent = ThompsonSamplingAgent(env=env, number_of_pulls=50000)
+random_agent = RandomAgent(env=env, number_of_pulls=10000)
+greedy_agent = GreedyAgent(env=env, number_of_pulls=10000)
+egreedy_agent = EpsilonGreedyAgent(env=env, number_of_pulls=10000) # Epsilon default value = 0.2.
+egd_agent = EGDecayAgent(env=env, number_of_pulls=10000) # Epsilon default = 1, Epsilon decay rate default = 0.99, Epsilon min default = 0.1
+ucb_agent = UCBAgent(env=env, number_of_pulls=10000) # Default c hyperparameter = 2.
+ts_agent = ThompsonSamplingAgent(env=env, number_of_pulls=10000)
+sm_agent = SoftMaxAgent(env=env, number_of_pulls=10000) # Default temperature value 0.01
 
 # Tell the agent to perform its actions, this returns a dictionary of metrics.
 ra_performance = random_agent.perform_actions()
@@ -37,6 +39,7 @@ ega_performance = egreedy_agent.perform_actions()
 egda_performance = egd_agent.perform_actions()
 ucb_performance = ucb_agent.perform_actions()
 ts_performance = ts_agent.perform_actions()
+sm_performance = sm_agent.perform_actions()
 
 # #---- Results and Plotting ----#
 
@@ -48,6 +51,7 @@ print(f"Epsilon Greedy Total Reward: {sum(ega_performance['rewards'])}")
 print(f"Epsilon Greedy Decay Total Reward: {sum(egda_performance['rewards'])}")
 print(f"UCB Total Reward: {sum(ucb_performance['rewards'])}")
 print(f"Thompson Sampling Total Reward: {sum(ts_performance['rewards'])}")
+print(f"SoftmaxTotal Reward: {sum(sm_performance['rewards'])}")
 
 # Plotting all of the metrics from each agent separately.
 plotting.plot_all_single(ra_performance, agent_type="Random_Agent")
@@ -57,14 +61,18 @@ plotting.plot_all_single(performance=egda_performance, agent_type="Epsilon_Greed
 plotting.plot_epsilon_decay(performance=egda_performance, agent_type="Epsilon_Greedy_with_Decay_Agent")
 plotting.plot_all_single(performance=ucb_performance, agent_type="UCB_Agent")
 plotting.plot_all_single(performance=ts_performance, agent_type="Thompson_Sampling_Agent")
+plotting.plot_all_single(performance=sm_performance, agent_type='Softmax_Agent')
 
 # Plotting comparative plots, comparing metrics from each agent together.
-agents_performance = [ra_performance, ga_performance, ega_performance, egda_performance, ucb_performance, ts_performance]
-names = ["Random Agent", 'Greedy Agent', 'epsilon-Greedy Agent', 'epsilon-Greedy Decay Agent', 'UCB Agent', 'Thompson Sampling Agent']
+agents_performance = [ra_performance, ga_performance, ega_performance, egda_performance,
+        ucb_performance, ts_performance, sm_performance]
+
+names = ["Random Agent", 'Greedy Agent', 'epsilon-Greedy Agent', 'epsilon-Greedy Decay Agent', 
+        'UCB Agent', 'Thompson Sampling Agent', 'Softmax Agent']
 plotting.cumulative_reward_compare_plot(agent_performance=agents_performance, names=names)
 plotting.total_reward_comparison(agent_performance=agents_performance, names=names)
 plotting.total_regret_comparison(agent_performance=agents_performance, names=names)
 
-agents_with_q = [ega_performance, ucb_performance, ga_performance]
-names = ['epsilon-Greedy', 'UCB', 'Greedy', 'True Values']
-plotting.action_value_comparison_plot(agent_performance=agents_with_q, names=names, true_vals=reward_probas)
+# agents_with_q = [ega_performance, ucb_performance, ga_performance]
+# names = ['epsilon-Greedy', 'UCB', 'Greedy', 'True Values']
+# plotting.action_value_comparison_plot(agent_performance=agents_with_q, names=names, true_vals=reward_probas)
